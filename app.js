54472ad0f118ec7214bcb70966ea0e6c9a7ec5f1
Richard Muñoz <richard.munoz@crifa.com>
2019-12-19 17:25:13 -0500

    added mongo connection parameter

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(
 mongoose
   .connect(
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-hdqj8.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
-    { useNewUrlParser: true }
+    { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(result => {
     // console.log(result);
