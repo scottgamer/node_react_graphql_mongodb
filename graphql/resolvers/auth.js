8cdc778781255e22eb888d81516a88921ea37685
Richard Muñoz <richard.munoz@crifa.com>
2019-12-20 17:14:24 -0500

    added login resolver

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,5 +1,6 @@
 const User = require("../../models/user");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
   createUser: async args => {
@@ -19,5 +20,32 @@ module.exports = {
     } catch (error) {
       throw error;
     }
+  },
+
+  login: async ({ email, password }) => {
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new Error("User does not exist");
+    }
+
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      throw new Error("Password is not correct");
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user.id,
+        email: user.email
+      },
+      "somesecret",
+      { expiresIn: "1h" }
+    );
+
+    return {
+      userId: user.id,
+      token,
+      tokenExpiration: 1
+    };
   }
 };
