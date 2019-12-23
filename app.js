c25bf6028b1be280c75d3ccb86f3d80df2a80150
Richard Munoz <richosojason@msn.com>
2019-12-23 14:26:14 -0500

    added headers to server

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ const isAuth = require("./middleware/is-auth");
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(isAuth);
 
 app.use(
@@ -29,6 +41,6 @@ mongoose
   )
   .then(result => {
     // console.log(result);
-    app.listen(3000);
+    app.listen(8000);
   })
   .catch(err => console.log(err));
