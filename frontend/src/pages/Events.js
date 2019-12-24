05b8ef6043c6d274bc66ed025df4b408ce618592
Richard Munoz <richosojason@msn.com>
2019-12-23 21:05:26 -0500

    added events

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Modal from "../components/Modal/Modal";
+import Backdrop from "../components/Backdrop/Backdrop";
+
+import "./Events.css";
 
 const Events = () => {
+  const [createEvent, setCreateEvent] = useState(false);
+
+  const startCreateEventHandler = () => {
+    setCreateEvent(true);
+  };
+
+  const modalConfirmHandler = () => {
+    setCreateEvent(false);
+  };
+
+  const modalCancelHandler = () => {
+    setCreateEvent(false);
+  };
+
   return (
-    <div>
-      <h1>The Events Page</h1>
-    </div>
+    <React.Fragment>
+      {createEvent && <Backdrop />}
+      {createEvent && (
+        <Modal
+          title="Add Event"
+          canCancel
+          canConfirm
+          onCancel={modalCancelHandler}
+          onConfirm={modalConfirmHandler}
+        >
+          <p>Modal Content</p>
+        </Modal>
+      )}
+      <div className="events-control">
+        <p>Show your own Events!</p>
+        <button className="btn" onClick={startCreateEventHandler}>
+          Create Event
+        </button>
+      </div>
+    </React.Fragment>
   );
 };
 
